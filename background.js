97210069f943b8a6ff3c93e6be84e94211b306c0
Francisco Moreno <franciscomoreno@gmail.com>
2015-10-25 19:42:11 +0100

    background logic added

---
+++ background.js
@@ -0,0 +1,90 @@
+
+var session = new Session();
+
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    switch(request.type) {
+        case "addBug":
+            addAnnotation("Bug",request.name);
+            break;
+        case "addIdea":
+            addAnnotation("Idea",request.name);
+            break;
+        case "addNote":
+            addAnnotation("Note",request.name);
+            break;
+        case "addQuestion":
+            addAnnotation("Question",request.name);
+            break;
+        case "exportSessionCSV":
+            exportSessionCSV();
+            break;
+        case "clearSession":
+            clearSession();
+            break;
+    }
+    return true;
+});
+
+function addAnnotation(type, name){
+
+    var currentUrl;
+    var now = Date.now();
+
+    if(session.getAnnotations().length == 0) this.startSession();
+
+     chrome.tabs.query({currentWindow: true, active: true},
+      function(tabs){
+         currentUrl = tabs[0].url;
+         //alert(currentUrl);
+        switch(type){
+          case "Bug":
+            var newBug = new Bug(name,currentUrl, now);
+            session.addBug(newBug);
+            break;
+          case "Note":
+            var newNote = new Note(name,currentUrl, now);
+            session.addNote(newNote);
+            break;
+          case "Idea":
+            var newIdea = new Idea(name,currentUrl, now);
+            session.addIdea(newIdea);
+            break;
+          case "Question":
+            var newQuestion = new Question(name,currentUrl, now);
+            session.addQuestion(newQuestion);
+            break;
+        }
+      });
+}
+
+function startSession(){
+    var browser=get_browser_info();
+    // browser.name = 'Chrome'
+    // browser.version = '40'
+    var browserInfoString = browser.name + "_" + browser.version;
+
+    session = new Session(Date.now(),browserInfoString);
+};
+
+function clearSession(){
+    session.clearAnnotations();
+};
+
+function exportSessionCSV(){
+    var exportService = new ExportSessionCSV(session);
+    var csvData = exportService.getCSVData();
+
+    var browserInfo = session.getBrowserInfo();
+
+    //Take the timestamp of the first Annotation
+    var startDateTime = session.getStartDateTime().toString('yyyyMMdd_HHmm');
+
+    var fileName = "ExploratorySession_" + browserInfo + "_" + startDateTime + ".csv";
+
+    var pom = document.createElement('a');
+    var blob = new Blob([csvData],{type: 'text/csv;charset=utf-8;'});
+    var url = URL.createObjectURL(blob);
+    pom.href = url;
+    pom.setAttribute('download', fileName);
+    pom.click();
+}
--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
-var session = new Session();
+//var session = new Session();
+var currentUrl;
 
 function showBugReport(){
     hideAllReports();
@@ -19,127 +20,114 @@ function showQuestionReport(){
 };
 
 document.addEventListener('DOMContentLoaded', function() {
-  var bugBtn = document.getElementById('BugBtn');
-  bugBtn.addEventListener('click', function() {
-    hideAllReports();
-    document.getElementById('addNewBug').style.display = 'block'; // show clicked element
-  }, false);
+  var bugBtn = document.getElementById("BugBtn");
+  bugBtn.addEventListener('click', showBugReport);
 }, false);
 
 document.addEventListener('DOMContentLoaded', function() {
-
   var addNewBug = document.getElementById("addNewBugBtn");
   addNewBug.addEventListener('click', function() {
 
     var bugName = document.getElementById("newBugDescription").value;
     if(bugName == "") return;
 
-    var newBug = new Bug(bugName,getCurrentUrl(), Date.now());
+    chrome.extension.sendMessage({
+      type: "addBug",
+      name: bugName,
+    });
 
-    //if(session.getAnnotations().length == 0) this.startSession();
-
-    //session.addBug(newBug);
-
-    //alert(session.getAnnotations().length);
     clearAllReports();
     hideAllReports();
 
   }, false);
 }, false);
 
-function addNewBug(){
-    var bugName = document.getElementById("newBugDescription").value;
-    if(bugName == "") return;
+document.addEventListener('DOMContentLoaded', function() {
+  var noteBtn = document.getElementById("NoteBtn");
+  noteBtn.addEventListener('click', showNoteReport);
+}, false);
 
-    var newBug = new Bug(bugName,getCurrentUrl(), Date.now());
+document.addEventListener('DOMContentLoaded', function() {
+  var addNewNoteBtn = document.getElementById("addNewNoteBtn");
+  addNewNoteBtn.addEventListener('click', function() {
 
-    if(session.getAnnotations().length == 0) this.startSession();
-
-    session.addBug(newBug);
-
-    //alert(session.getAnnotations().length);
-
-    clearAllReports();
-    hideAllReports();
-
-};
-function addNewNote(){
     var noteName = document.getElementById("newNoteDescription").value;
     if(noteName == "") return;
 
-    var newNote = new Note(noteName,getCurrentUrl(), Date.now());
-
-    if(session.getAnnotations().length == 0) this.startSession();
-
-    session.addNote(newNote);
-
-    clearAllReports();
-    hideAllReports();
-
-};
-function addNewIdea(){
-    var ideaName = document.getElementById("newIdeaDescription").value;
-    if(ideaName == "") return;
-
-    var newIdea = new Idea(ideaName,getCurrentUrl(), Date.now());
-
-    if(session.getAnnotations().length == 0) this.startSession();
-
-    session.addIdea(newIdea);
+    chrome.extension.sendMessage({
+      type: "addNote",
+      name: noteName,
+    });
 
     clearAllReports();
     hideAllReports();
 
-};
-function addNewQuestion(){
+  }, false);
+}, false);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var questionBtn = document.getElementById("QuestionBtn");
+  questionBtn.addEventListener('click', function() {
+   showQuestionReport();
+  }, false);
+}, false);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var addNewQuestionBtn = document.getElementById("addNewQuestionBtn");
+  addNewQuestionBtn.addEventListener('click', function() {
     var questionName = document.getElementById("newQuestionDescription").value;
     if(questionName == "") return;
-
-    var newQuestion = new Question(questionName,getCurrentUrl(), Date.now());
-
-    if(session.getAnnotations().length == 0) this.startSession();
-
-    session.addQuestion(newQuestion);
+    chrome.extension.sendMessage({
+      type: "addQuestion",
+      name: questionName
+    });
 
     clearAllReports();
     hideAllReports();
 
+  }, false);
+}, false);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var ideaBtn = document.getElementById("IdeaBtn");
+  ideaBtn.addEventListener('click', function() {
+   showIdeaReport();
+  }, false);
+}, false);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var addNewIdeaBtn = document.getElementById("addNewIdeaBtn");
+  addNewIdeaBtn.addEventListener('click', function() {
+    var ideaName = document.getElementById("newIdeaDescription").value;
+    if(ideaName == "") return;
+    chrome.extension.sendMessage({
+      type: "addIdea",
+      name: ideaName
+    });
+
+    clearAllReports();
+    hideAllReports();
+
+  }, false);
+}, false);
+
+function exportSessionCSV(){
+   chrome.extension.sendMessage({
+      type: "exportSessionCSV"
+   });
 };
 
-function exportSesstionCSV(){
-    if(session.getAnnotations().length == 0) return;
+document.addEventListener('DOMContentLoaded', function() {
+  var exportCSVBtn = document.getElementById("exportCSVBtn");
+  exportCSVBtn.addEventListener('click',  exportSessionCSV)
+}, false);
 
-    var exportService = new ExportSessionCSV(session);
-    var csvData = exportService.getCSVData();
-
-    var browserInfo = session.getBrowserInfo();
-
-    //Take the timestamp of the first Annotation
-    var startDateTime = session.getStartDateTime().toString('yyyyMMdd_HHmm');
-
-    //var fileName = "test" + ".csv";
-    var fileName = "ExploratorySession_" + browserInfo + "_" + startDateTime + ".csv";
-
-    var pom = document.createElement('a');
-    var blob = new Blob([csvData],{type: 'text/csv;charset=utf-8;'});
-    var url = URL.createObjectURL(blob);
-    pom.href = url;
-    pom.setAttribute('download', fileName);
-    pom.click();
-};
-
-function startSession(){
-    var browser=get_browser_info();
-    // browser.name = 'Chrome'
-    // browser.version = '40'
-    var browserInfoString = browser.name + "_" + browser.version;
-
-    session = new Session(Date.now(),browserInfoString);
-};
-
-function clearSession(){
-    session.clearAnnotations();
-};
+document.addEventListener('DOMContentLoaded', function() {
+   var cancelAnnotationBtn = document.getElementsByName("Cancel");
+   for (var i = 0; i < cancelAnnotationBtn.length; i++) {
+       cancelAnnotationBtn[i].addEventListener('click', cancelAnnotation);
+   }
+}, false);
 
 function cancelAnnotation(){
    clearAllReports();
@@ -159,3 +147,21 @@ function hideAllReports(){
     document.getElementById("addNewNote").style.display = 'none';
     document.getElementById("addNewQuestion").style.display = 'none';
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+  var previewBtn = document.getElementById('previewBtn');
+  previewBtn.addEventListener('click', function() {
+   var background = chrome.extension.getBackgroundPage();
+   var session = background.session;
+   alert(session.getAnnotations().length)
+  }, false);
+}, false);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var resetBtn = document.getElementById('resetBtn');
+  resetBtn.addEventListener('click', function() {
+    chrome.extension.sendMessage({
+       type: "clearSession"
+    });
+  }, false);
+}, false);
