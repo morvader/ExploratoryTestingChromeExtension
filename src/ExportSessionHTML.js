a882682faff171bbc2c4002700ed5d1d5a7792db
Francisco Moreno <franciscomoreno@gmail.com>
2015-11-20 08:30:14 +0100

    Added filters and styles to Exportable HTML Report

--- src/ExportSessionHTML.js
+++ src/ExportSessionHTML.js
@@ -11,22 +11,35 @@ ExportSessionHTML.prototype.getHTML = function(fileName) {
     meta.content = 'text/html; charset=utf-8';
     doc.head.appendChild(meta);
 
-    addStyleCSS(doc);
+    this.addStyleCSS(doc);
+
+    this.addJavaScriptFiles(doc);
+
 
     var h1 = doc.createElement("h1");
     h1.innerHTML = fileName;
 
     doc.body.appendChild(h1);
 
-    createExportableTable(doc);
+    this.createExportableTable(doc);
 
+    this.addTableFilters(doc);
+
+    doc.body.setAttribute('onload', "loadFilter()");
 
     return doc;
 }
 
+ExportSessionHTML.prototype.addJavaScriptFiles = function(doc) {
+    //For table Filter
+    var tableFilterScript = document.createElement('script');
+    tableFilterScript.type = 'text/javascript';
+    tableFilterScript.src = 'http://tablefilter.free.fr/TableFilter/tablefilter_all_min.js';
 
+    doc.getElementsByTagName('head')[0].appendChild(tableFilterScript);
+}
 
-function createExportableTable(doc) {
+ExportSessionHTML.prototype.createExportableTable = function(doc) {
 
         var tableDiv = doc.createElement("div");
         doc.body.appendChild(tableDiv);
@@ -35,7 +48,7 @@ function createExportableTable(doc) {
 
         var table = document.createElement('TABLE');
 
-        table.setAttribute('id', 'exportActivityTable');
+        table.setAttribute('id', "exportActivityTable");
         table.style.width = "100%";
 
         var caption = document.createElement("caption");
@@ -118,7 +131,7 @@ function createExportableTable(doc) {
 
 }
 
-function addStyleCSS(doc){
+ExportSessionHTML.prototype.addStyleCSS = function(doc){
     var css = document.createElement("style");
     css.type = "text/css";
     css.innerHTML = "#exportActivityTable { \
@@ -152,18 +165,21 @@ function addStyleCSS(doc){
                      #exportActivityTable tr:nth-child(even) { \
                      background: #F2F2F2; \
                      } \
-                     .centered{ \
-                         text-align:center; \
-                     } \
-                     td{ \
-                        vertical-align: top; \
-                     } \
-                     .noWrap{ \
-                      white-space: nowrap; \
-                     } \
-                     TH{ \
-                         padding:5px; \
-                     } \
+                     .centered{text-align:center;} \
+                     td{vertical-align: top;} \
+                     .noWrap{white-space: nowrap;} \
+                     TH{padding:5px;} \
+                     body{ font-family: 'Segoe UI', Tahoma, sans-serif;} \
      ";
     doc.head.appendChild(css);
+}
+
+ExportSessionHTML.prototype.addTableFilters = function(doc){
+    var script = document.createElement("script");
+
+    // Add script content
+
+    script.innerHTML = "function loadFilter(){ var sessionActivityTable_Props = {col_0: 'select',col_1: 'none',col_2: 'none',col_3: 'none',custom_cell_data_cols: [0],display_all_text: ' [ Show all ] ',sort_select: true};var tf2 = setFilterGrid('exportActivityTable', sessionActivityTable_Props);} ";
+
+    doc.getElementsByTagName('head')[0].appendChild(script);
 }
